dfa309fa466645f2b0803994d86a652e8e470daa
Stefan Hendricks <stefan.hendricks@awi.de>
2019-05-28 17:28:16 +0200

    [refactoring] added separate classes for the header and data

--- awi_als_toolbox/reader.py
+++ awi_als_toolbox/reader.py
@@ -11,23 +11,10 @@ from collections import OrderedDict
 
 import struct
 import logging
-import warnings
 
 
-class ALSL1BFileDefinition():
-    """ Data class containing the definitions of the header data in the ALS """
-
-    # Header information of the form (variable_name, [number of bytes, struct format])
-    header_dict = OrderedDict((('scan_lines', [4, '>L']),
-                               ('data_points_per_line', [2, '!H']),
-                               ('bytes_per_line', [2, '>H']),
-                               ('bytes_sec_line', [8, '>Q']),
-                               ('year', [2, '>H']),
-                               ('month', [1, '>b']),
-                               ('day', [1, '>b']),
-                               ('start_time_sec', [4, '>L']),
-                               ('stop_time_sec', [4, '>L']),
-                               ('device_name', [8, '>8s'])))
+class AirborneLaserScannerFile(object):
+    """ Class to retrieve data from a AWI ALS binary data file """
 
     # Variable names and their data type
     line_variables = OrderedDict((('timestamp', np.float64),
@@ -37,129 +24,94 @@ class ALSL1BFileDefinition():
                                   ('amplitude', np.float32),
                                   ('reflectance', np.float32)))
 
-    def __init__(self):
-        """ """
-        pass
 
+    def __init__(self, filepath):
+        """
+        Connects to a AWI binary ALS data file. The data is not parsed into memory when this is class is called,
+        only the header information that is necessary to decode the binary data structure.
 
-class AirborneLaserScannerFile(object):
-    """ Not more than a proof of concept yet """
+        Usage:
+        ======
 
-    def __init__(self):
-        self.filename = None
-        self.header = ALSL1BHeader()
+            alsfile = AirborneLaserScannerFile(filename)
+            als = alsfile.get_data(start, stop)
+
+        :param filepath: (str) The path of the AWI ALS file
+        """
+
+        # Store Parameter
+        self.filepath = filepath
+
+        # Decode and store header information
+        self.header = ALSFileHeader(filepath)
         self.line_timestamp = None
-        self.connected = False
-        self.n_selected_lines = 0
-        self.filedef = ALSL1BFileDefinition()
 
-    def connect(self, filename):
-        """
-        Connect to ALS Level1b binary file: retrieve header information
-        """
-        self.filename = filename
-        self.read_header()
-        self.read_line_timestamp()
-        self.connected = True
-        self.set_full_time_range()
+        # Read the line timestamp
+        # -> on timestamp per line to later select subsets of the full content
+        self._read_line_timestamp()
 
-    def read_header(self, verbose=False):
+    def get_data(self, start_seconds=None, end_seconds=None):
         """
-        Read the header of the ALS level 1b file
-        :param verbose: (bool) Flag that control the level of messages to stdio
-        :return: 
+        Read a subset of the ALS data and return its content. The subset is selected with the (integer) seconds of
+        the day. If `start_seconds` and `end_seconds` are omitted, the maximum range will be used
+        :param start_seconds: (int) Start of the subset in seconds of the day
+        :param end_seconds: (int) End of the subset in seconds of the day
+        :return: an ALSData object containing the data subset
         """
 
-        # Get the header information
-        header_dict = self.filedef.header_dict
+        # Check input
+        if start_seconds is None:
+            start_seconds = self.line_timestamp[0]
 
-        with open(self.filename, 'rb') as f:
+        if end_seconds is None:
+            end_seconds = self.line_timestamp[-1]
 
-            # Read header size
-            self.header.byte_size = struct.unpack('>b', f.read(1))[0]
-            logging.info("als_header.byte_size: %s" % str(self.header.byte_size))
-            if self.header.byte_size == 36:
-                header_dict['data_points_per_line'] = [1, '>B']
-            elif self.header.byte_size == 37:
-                header_dict['data_points_per_line'] = [2, '>H']
-            else:
-                msg = "Unkown ALS L1B header size: %g (Should be 36 or 37 or unsupported Device)"
-                msg = msg % self.header.byte_size,
-                raise ValueError(msg)
+        # Sanity check
+        self.validate_time_range(start_seconds, end_seconds)
 
-            # Read Rest of header
-            for key in header_dict.keys():
-                nbytes, fmt = header_dict[key][0], header_dict[key][1]
-                setattr(self.header, key,
-                        struct.unpack(fmt, f.read(nbytes))[0])
-                if verbose:
-                    logging.info("als_header.%s: %s" % (key, str(getattr(self.header, key))))
+        # Get the number of lines
+        line_index = [
+            np.where(self.line_timestamp >= start_seconds)[0][0],
+            np.where(self.line_timestamp <= end_seconds)[0][-1]]
+        n_selected_lines = line_index[1] - line_index[0]
 
-    def read_line_timestamp(self):
-        """ Read the line time stamp """
-        with open(self.filename, 'rb') as f:
-            f.seek(self.get_start_byte_linetimestamp())
-            data = f.read(self.header.bytes_sec_line)
-        struct_def = ">{scan_lines}L".format(scan_lines=self.header.scan_lines)
-        self.line_timestamp = np.array(struct.unpack(struct_def, data))
+        # Get the section of the file to read
+        startbyte, nbytes = self._get_data_bytes(line_index)
 
-    def read_data(self):
-        """  Read lines defined by start and stop second of day """
-        self.validate_file()
-        startbyte, nbytes = self.get_data_bytes()
-#        bindat_dtype = "s%s" % str(nbytes)
-        bindat = np.ndarray(shape=(self.n_selected_lines), dtype=object)
-        with open(self.filename, 'rb') as f:
-            for i in np.arange(self.n_selected_lines):
+        # Get the shape of the output array
+        nlines, nshots = n_selected_lines, self.header.data_points_per_line
+
+        # Init the data output
+        als = ALSData(self.line_variables, (nlines, nshots))
+
+        # Read the binary data
+        bindat = np.ndarray(shape=(nlines), dtype=object)
+        with open(self.filepath, 'rb') as f:
+            for i in np.arange(n_selected_lines):
                 f.seek(startbyte)
                 bindat[i] = f.read(nbytes)
                 startbyte += nbytes
-            # bindat = np.fromfile(f, count=nbytes)
-        self.set_data_variables()
-        self.data = self.unpack_binary_line_data(bindat)
 
-    def unpack_binary_line_data(self, bindat):
-        """ TODO: working solution, but needs to be improved """
-        nlines = self.n_selected_lines
-        nshots = self.header.data_points_per_line
+        # Unpack the binary data
         start_byte, stop_byte = 0, self.header.bytes_per_line
         for i in np.arange(nlines):
             line = bindat[i]
             i0, i1 = 0, 8*nshots
-            self.timestamp[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
+            als.timestamp[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
             i0 = i1
             i1 = i0 + 8*nshots
-            self.latitude[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
+            als.latitude[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
             i0 = i1
             i1 = i0 + 8*nshots
-            self.longitude[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
+            als.longitude[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
             i0 = i1
             i1 = i0 + 8*nshots
             start_byte += self.header.bytes_per_line
             stop_byte += self.header.bytes_per_line
-            self.elevation[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
+            als.elevation[i, :] = struct.unpack(">{n}d".format(n=nshots), line[i0:i1])
 
-    def set_data_variables(self):
-        """ Create the numpy arrays for unpacking of binary line data """
-        nlines = self.n_selected_lines
-        nshots = self.header.data_points_per_line
-        for key in self.filedef.line_variables.keys():
-            setattr(self, key, np.ndarray(shape=(nlines, nshots),dtype=self.filedef.line_variables[key]))
-
-    def set_time_range(self, time_range):
-        """ Sets the first and last line of the subsection """
-        self.validate_file()
-        self.validate_time_range(time_range[0], time_range[1])
-        self.line_index = [
-            np.where(self.line_timestamp >= time_range[0])[0][0],
-            np.where(self.line_timestamp <= time_range[1])[0][-1]]
-        self.n_selected_lines = self.line_index[1] - self.line_index[0]
-
-    def set_full_time_range(self):
-        """ Set the full time range as selected content """
-        self.validate_file()
-        self.line_index = [0, self.header.scan_lines-1]
-        self.n_selected_lines = self.header.scan_lines
+        # All done, return
+        return als
 
     def validate_time_range(self, start, stop):
         """ Check for oddities in the time range selection """
@@ -178,46 +130,101 @@ class AirborneLaserScannerFile(object):
         # Raise Warnings
         if start < fstart:
             # TODO: Use logging
-            warnings.warn("start time {start} before actual start of " +
-                          "file {fstart}".format(start=start, fstart=fstart))
+            logging.warn("start time {start} before actual start of file {fstart}".format(start=start, fstart=fstart))
         if stop > fstop:
-            warnings.warn("stop time {stop} after actual end of file " +
-                          "{fstop}".format(stop=stop, fstop=fstop))
+            logging.warn("stop time {stop} after actual end of file {fstop}".format(stop=stop, fstop=fstop))
 
-    def validate_file(self):
-        """ Check if file has been specified correctly """
-        if not self.connected:
-            raise IOError("not connected to file -> self.connect(filename)")
+    def _get_data_bytes(self, line_index):
+        """
+        Computes the start byte and the number of bytes to read for the given lines
+        :param line_index: (array) list of scan lines to be read
+        :return: (int, int) startbyte and the number of bytes for the data subset
+        """
 
-    def get_data_bytes(self):
-        """ Returns start and stop bytes of the selected data section """
         # Start byte of scan line
         startbyte = np.uint32(self.header.byte_size)
         startbyte += np.uint32(self.header.bytes_sec_line)
-        startbyte += np.uint32(self.line_index[0]) * np.uint32(self.header.bytes_per_line)
+        startbyte += np.uint32(line_index[0]) * np.uint32(self.header.bytes_per_line)
+
         # Number bytes for selected scan lines
-        # n_scan_lines = self.line_index[1]-self.line_index[0]
-        nbytes = self.header.bytes_per_line  # * n_scan_lines
+        nbytes = self.header.bytes_per_line
+
         return startbyte, nbytes
 
-    def get_start_byte_linetimestamp(self):
-        """ Returns the start byte of the line timestamp array """
-        return self.header.byte_size
+    def _read_line_timestamp(self):
+        """ Read the line time stamp """
+        with open(self.filepath, 'rb') as f:
+            f.seek(self.header.byte_size)
+            data = f.read(self.header.bytes_sec_line)
+        struct_def = ">{scan_lines}L".format(scan_lines=self.header.scan_lines)
+        self.line_timestamp = np.array(struct.unpack(struct_def, data))
 
-    def get_center_beam_index(self):
+
+class ALSFileHeader(object):
+    """ Class for parsing and storing header information of binary AWI ALS data files """
+
+    # Header information of the form (variable_name, [number of bytes, struct format])
+    header_dict = OrderedDict((('scan_lines', [4, '>L']),
+                               ('data_points_per_line', [2, '!H']),
+                               ('bytes_per_line', [2, '>H']),
+                               ('bytes_sec_line', [8, '>Q']),
+                               ('year', [2, '>H']),
+                               ('month', [1, '>b']),
+                               ('day', [1, '>b']),
+                               ('start_time_sec', [4, '>L']),
+                               ('stop_time_sec', [4, '>L']),
+                               ('device_name', [8, '>8s'])))
+
+    def __init__(self, filepath, verbose=True):
+        """
+        Decode and store header information from binary AWI ALS files
+        :param filepath: (str) The path to the ALS file
+        :param verbose: (bool) Flag determining the verbosity
+        """
+
+        # Read the header
+        with open(filepath, 'rb') as f:
+
+            # Read header size
+            self.byte_size = struct.unpack('>b', f.read(1))[0]
+            logging.info("als_header.byte_size: %s" % str(self.byte_size))
+            if self.byte_size == 36:
+                self.header_dict['data_points_per_line'] = [1, '>B']
+            elif self.byte_size == 37:
+                self.header_dict['data_points_per_line'] = [2, '>H']
+            else:
+                msg = "Unkown ALS L1B header size: %g (Should be 36 or 37 or unsupported Device)"
+                msg = msg % self.byte_size,
+                raise ValueError(msg)
+
+            # Read Rest of header
+            for key in self.header_dict.keys():
+                nbytes, fmt = self.header_dict[key][0], self.header_dict[key][1]
+                setattr(self, key, struct.unpack(fmt, f.read(nbytes))[0])
+                if verbose:
+                    logging.info("als_header.%s: %s" % (key, str(getattr(self, key))))
+
+    @property
+    def center_beam_index(self):
         """ Returns index of the center beam """
-        if not self.connected:
-            return -1
-        return np.median(np.arange(self.header.data_points_per_line))
-
-    def get_n_shots_per_line(self):
-        """ Returns index of the center beam """
-        if not self.connected:
-            return 0
-        return self.header.data_points_per_line
+        return np.median(np.arange(self.data_points_per_line))
 
 
-class ALSL1BHeader(object):
+class ALSData(object):
+    """ A data class container for ALS data"""
 
-    def __init__(self):
-        pass
+    def __init__(self, vardef, shape):
+        """
+        Data container for ALS data ordered in scan lines.
+        NOTE: Upon initialization this container will be empty. The content must be added directly.
+        :param filedef: (dict) Variable definition {varname: dtype, ... }
+        :param shape: The shape of the (nlines, nshots) of the data
+        """
+
+        # Store arguments
+        self.vardef = vardef
+        self.shape = shape
+
+        # Create the array entries
+        for key in vardef.keys():
+            setattr(self, key, np.ndarray(shape=shape, dtype=vardef[key]))
