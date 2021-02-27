90b1608ca9d052713609a00e64129ea909cba6cf
Stefan Hendricks <stefan.hendricks@googlemail.com>
2021-02-27 15:56:26 +0100

    update package file

--- awi_als_toolbox/__init__.py
+++ awi_als_toolbox/__init__.py
@@ -3,22 +3,25 @@
 """
 """
 
-__all__ = ["export", "data", "demgen", "graphics", "reader", "utils"]
+__all__ = ["export", "demgen", "graphics", "scripts",
+           "AirborneLaserScannerFile", "AirborneLaserScannerFileV2"]
 
 import os
 import sys
+
+from ._reader import (AirborneLaserScannerFile, AirborneLaserScannerFileV2)
 
 import warnings
 warnings.filterwarnings("ignore")
 
 # Get version from VERSION in package root
 PACKAGE_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+version_file = open(os.path.abspath(os.path.join(PACKAGE_ROOT_DIR, "VERSION")))
 try:
-    version_file = open(os.path.abspath(os.path.join(PACKAGE_ROOT_DIR, "VERSION")))
     with version_file as f:
         version = f.read().strip()
 except IOError:
-    sys.exit("Cannot find VERSION file in package (expected: %s" % version_file)
+    sys.exit("Cannot find VERSION file in package (expected: {}".format(version_file))
 
 
 # Package Metadata
