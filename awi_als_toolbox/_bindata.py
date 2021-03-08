Fixed ice drift correction for parallel computation

awi_als_toolbox/_bindata.py
@@ -537,8 +537,8 @@ class ALSPointCloudData(object):
         self._set_metadata()
 
     def init_IceDriftCorrection(self):
-        self.x = np.empty(self.longitude.shape)*np.NaN
-        self.y = np.empty(self.longitude.shape)*np.NaN
+        self.x = np.empty(self.get("longitude").shape)*np.NaN
+        self.y = np.empty(self.get("longitude").shape)*np.NaN
         self.IceDriftCorrected = False
         self.IceCSTransform = None
 

awi_als_toolbox/filter.py
@@ -118,11 +118,11 @@ class IceDriftCorrection(ALSPointCloudFilter):
         als.init_IceDriftCorrection()
 
         # 3. mask nan values for faster computation
-        nonan = np.where(np.logical_or(np.isfinite(als.longitude), np.isfinite(als.latitude)))
+        nonan = np.where(np.logical_or(np.isfinite(als.get("longitude")), np.isfinite(als.get("latitude"))))
 
         # 4. Generate GeoPositionData object from als
-        time_als = np.array([datetime(1970,1,1,0,0,0) + timedelta(0,isec) for isec in als.timestamp[nonan]])
-        als_geo_pos = GeoPositionData(time_als,als.longitude[nonan],als.latitude[nonan])
+        time_als = np.array([datetime(1970,1,1,0,0,0) + timedelta(0,isec) for isec in als.get("timestamp")[nonan]])
+        als_geo_pos = GeoPositionData(time_als,als.get("longitude")[nonan],als.get("latitude")[nonan])
 
         # 5. Compute projection
         icepos, als.IceCSTransform = self.IceCoordinateSystem.get_xy_coordinates(als_geo_pos, transform_output=True)